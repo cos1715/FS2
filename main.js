07ded332d944b1821c5d8b3b9466c461bacc9dba
Taras Samoilenko <tsamoilenko97@gmail.com>
2024-03-12 21:54:35 +0200

    created js file

---
+++ main.js
@@ -0,0 +1,17 @@
+/* 
+this file is about taking user name
+and user age
+*/
+
+// alert("Hello world");
+// this part of program takes info about user
+{
+  // debugger;
+  const userName = prompt("Please enter your name"); // this works ok
+  const userAge = +prompt("Please enter your age");
+  const fullAge = userAge > 17;
+
+  console.log(userName);
+  console.log(userAge);
+  console.log(fullAge);
+}
